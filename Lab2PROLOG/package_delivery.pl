380aad92ebdfab337c940da7f5d5b52f55a5b801
Limpowitch <timrosendahl2@gmail.com>
2025-05-12 08:38:50 +0200

    Lab 2 PROLOG done

---
+++ Lab2PROLOG/package_delivery.pl
@@ -0,0 +1,91 @@
+% Move from R1 to R2
+move(
+    state(r1, holding, Brasskey, Package, TotalItems),
+    walk(r1, r2),
+    state(r2, holding, Brasskey, Package, TotalItems)
+    ).
+
+% Move from R2 to R1
+move(
+    state(r2, holding, Brasskey, Package, TotalItems),
+    walk(r2, r1),
+    state(r1, holding, Brasskey, Package, TotalItems)
+    ).
+
+% Move from R1 to R3
+move(
+    state(r1, Steelkey, holding, Package, TotalItems),
+    walk(r1, r3),
+    state(r3, Steelkey, holding, Package, TotalItems)
+    ).
+
+% Move from R3 to R1
+move(
+    state(r3, Steelkey, holding, Package, TotalItems),
+    walk(r3, r1),
+    state(r1, Steelkey, holding, Package, TotalItems)
+    ).
+
+%Grab Steel Key
+grab(
+    state(CurrentRoom, CurrentRoom, Brasskey, Package, TotalItems),
+    grab(steelkey, CurrentRoom),
+    state(CurrentRoom, holding, Brasskey, Package, NewTotalItems)) :-
+    TotalItems < 2,
+    NewTotalItems is TotalItems + 1.
+
+%Grab Brass Key
+grab(
+    state(CurrentRoom, Steelkey, CurrentRoom, Package, TotalItems),
+    grab(brasskey, CurrentRoom),
+    state(CurrentRoom, Steelkey, holding, Package, NewTotalItems)) :-
+    TotalItems < 2,
+    NewTotalItems is TotalItems + 1.
+
+%Grab Package
+grab(
+    state(CurrentRoom, Steelkey, Brasskey, CurrentRoom, TotalItems),
+    grab(package, CurrentRoom),
+    state(CurrentRoom, Steelkey, Brasskey, holding, NewTotalItems)) :-
+    TotalItems < 2,
+    NewTotalItems is TotalItems + 1.
+
+%Drop Steel Key
+drop(
+    state(CurrentRoom, holding, Brasskey, Package, TotalItems),
+    drop(steelkey, CurrentRoom),
+    state(CurrentRoom, CurrentRoom, Brasskey, Package, NewTotalItems)) :-
+    NewTotalItems is TotalItems - 1.
+
+%Drop Brass Key
+drop(
+    state(CurrentRoom, Steelkey, holding, Package, TotalItems),
+    drop(brasskey, CurrentRoom),
+    state(CurrentRoom, Steelkey, CurrentRoom, Package, NewTotalItems)) :-
+    NewTotalItems is TotalItems - 1.
+
+%Drop Package
+drop(
+    state(CurrentRoom, Steelkey, Brasskey, holding, TotalItems),
+    drop(package, CurrentRoom),
+    state(CurrentRoom, Steelkey, Brasskey, CurrentRoom, NewTotalItems)) :-
+    NewTotalItems is TotalItems - 1.
+
+solveR(Start, MaxDepth, Moves) :-
+    solveR(Start, MaxDepth, [Start], Moves),
+    !.
+
+solveR(state(_,_,_,r2,_), _, _, []).
+
+solveR(State, N, Visited, [Action|Rest]) :-
+    N > 0,
+    (   move(State,   Action, Next)
+    ;   grab(State,   Action, Next)
+    ;   drop(State,   Action, Next)
+    ),
+    \+ member(Next, Visited),
+    N1 is N - 1,
+    solveR(Next, N1, [Next|Visited], Rest).
+
+% invoke with: solveR(state(r1,r1,r2,r3,0), 20, Moves), write_term(Moves, [max_depth(0)]), nl.
+    
