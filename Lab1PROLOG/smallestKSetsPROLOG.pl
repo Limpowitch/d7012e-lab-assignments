e985fb463401a84dc7e9b5858590c7a30bceb770
Limpowitch <timrosendahl2@gmail.com>
2025-05-12 10:21:32 +0200

    Added comments to Lab 1 PROLOG

--- Lab1PROLOG/smallestKSetsPROLOG.pl
+++ Lab1PROLOG/smallestKSetsPROLOG.pl
@@ -1,46 +1,41 @@
-
-% Our core compound term to pass around all core information
-% sum_slice(Sum, Slice, Start, End) 
-
-contiguous_sublist_indexed(List, Slice, Start, End) :-
-    append(Prefix, Tail, List),
+contiguous_sublist_indexed(List, Slice, Start, End) :-  % Example, [1,2,3] (first instance, no rollback)
+    append(Prefix, Tail, List),                         % Prefix = [], Tail = [1,2,3]
     length(Prefix, PreLen),
     Start is PreLen + 1,
-    append(Slice, _, Tail),
+    append(Slice, _, Tail),                             % Append _ to Slice. Slice = [], Tail = [1,2,3]. Rollback applies here
     length(Slice, Len),
     End is PreLen + Len.
-
-contiguous_sublist(List, Slice) :-
-    append(_, Tail, List),
-    append(Slice, _, Tail).
 
 all_sublists_indexed(List, Triplets) :-
     findall(
         slice_info(Slice, Start, End),
         (   
             contiguous_sublist_indexed(List, Slice, Start, End),
-            Slice \= []
+            Slice \= []                                 % We make sure to remove empty sublists
         ),
         Triplets
     ).    
 
-all_subset_sums(SliceInfos, SumSlices) :-
+all_subset_sums(IndexedSublists, SumSlices) :-
     findall(
         sum_slice(Sum, Slice, Start, End),
-        (member(slice_info(Slice, Start, End), SliceInfos),
-            sumlist(Slice, Sum)
+        (
+            member(slice_info(Slice, Start, End), IndexedSublists), 
+            sumlist(Slice, Sum)                         % We check if member, if yes, we sum all elements in the Slice
         ),
-        SumSlices
+        SumSlices                                       % We return SumSlices at the end
     ).
 
 sort_slices_by_sum(SumSlices, Sorted) :-
     maplist(
-        [sum_slice(Sum, Slice, Start, End), Sum-sum_slice(Sum, Slice, Start, End)]>>true,
-        SumSlices,
-        Pairs
+      [sum_slice(Sum, Slice, Start, End), Pair]>>(      % We convert the sumSlice to a key-value pair
+        Pair = Sum - sum_slice(Sum, Slice, Start, End)
+      ),
+      SumSlices,
+      Pairs                                             % Returns all pairs
     ),
-    keysort(Pairs,SortedPairs),
-    pairs_values(SortedPairs, Sorted).
+    keysort(Pairs, SortedPairs),
+    pairs_values(SortedPairs, Sorted).                  % We first sort by key-values (Sum), then drop the key and return the value
 
 print_rows([]).
 print_rows([sum_slice(Sum, Slice, Start, End) | Rest]) :-
@@ -49,11 +44,9 @@ print_rows([sum_slice(Sum, Slice, Start, End) | Rest]) :-
     print_rows(Rest).
 
 take(0, _List, []) :- !.
-take(K, [X|Xs], [X|Ys]) :-
-    K > 0,                
-    K1 is K - 1,          
-    take(K1, Xs, Ys).     
-take(_K, [], []).        
+take(K, List, Prefix) :-                              
+    length(Prefix, K),        
+    append(Prefix, _, List).  
 
 main_calculculations(List, Result) :-
     all_sublists_indexed(List, IndexedSublists),
@@ -62,6 +55,6 @@ main_calculculations(List, Result) :-
 
 main_print(List, K) :-
     main_calculculations(List, All),        
-    take(K, All, TopK),
+    take(K, All, TopK),                                 % Makes sure we only take the first K sorted elements
     writeln('size i j sublist'),
     print_rows(TopK).
